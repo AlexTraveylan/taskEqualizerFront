1d7cfad8ea2d6d81ce86ca83e1f7282e424aff43
AlexTraveylan <monsieurdemares@gmail.com>
2024-05-07 22:07:56 +0200

    invitation service

---
+++ lib/services/invitation.ts
@@ -0,0 +1,28 @@
+import { invitationUrl } from "@/lib/api-setting"
+import { Invitation, invitationSchema } from "@/lib/schema/invitation"
+
+export class InvitationService {
+  /**
+   * Creates an invitation.
+   * @returns A Promise that resolves to an instance of Invitation if successful, otherwise undefined.
+   */
+  async createInvitation(): Promise<Invitation | undefined> {
+    const response = await fetch(invitationUrl, {
+      method: "GET",
+      credentials: "include",
+    })
+
+    if (!response.ok) {
+      console.error("Failed to fetch invitation")
+      return
+    }
+
+    const data = await response.json()
+    try {
+      const parsedData = invitationSchema.parse(data)
+      return parsedData
+    } catch (error) {
+      console.error("Failed to parse invitation")
+    }
+  }
+}
